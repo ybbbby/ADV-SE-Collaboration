8f818f61d64b349cf578030199007c4135f3c4b9
DQ <dq2146@columbia.edu>
2020-11-06 01:25:23 -0500

    add search bar

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Route } from 'react-router-dom'
 import EventsNearby from './pages/EventsNearby/EventsNearby'
 import SimpleMap from './components/GoogleMap/SimpleMap'
+import LocationSearchInput from './components/GoogleMap/LocationSearchInput'
 
 export default function Routes() {
   return (
@@ -9,6 +10,7 @@ export default function Routes() {
       <Route exact path="/" component={EventsNearby} />
       <Route exact path="/eventsnearby/:user" component={EventsNearby} />
       <Route exact path="/map" component={SimpleMap} />
+      <Route exact path="/searchbar" component={LocationSearchInput} />
     </>
   )
 }
