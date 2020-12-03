84b431386a38a0ddab03ff0fca9c3c88574b1d19
DQ <dq2146@columbia.edu>
2020-12-03 15:38:28 -0500

    It should work, but not needed to pr.
    Hope somebody could verify those could work since I don't have two accounts..:P

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,22 +4,6 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 import App from './App'
 import theme from './theme'
-import g from './global'
-import GoEasy from 'goeasy'
-
-g.goEasy = new GoEasy({
-  host: 'hangzhou.goeasy.io',
-  appkey: 'BC-718ddbf6f6ef4fb5a6b0f6cb62814fc8',
-  onConnected: function () {
-    console.log('web socket connected')
-  },
-  onDisconnected: function () {
-    console.log('web socket disconnected')
-  },
-  onConnectFailed: function () {
-    console.log('web socket connection failed')
-  },
-})
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
